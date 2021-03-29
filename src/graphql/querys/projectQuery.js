28718df45a8de488f011449c422349febee8843c
senixcode <codesenix@gmail.com>
2021-03-28 20:51:30 -0500

    feat: :sparkles: added findByLanguage -> project

--- src/graphql/querys/projectQuery.js
+++ src/graphql/querys/projectQuery.js
@@ -2,17 +2,8 @@ import { PROJECT_FIELDS } from "../fragments/projectFields";
 import { gql } from "apollo-boost";
 export const GET_PROJECTS = gql`
   ${PROJECT_FIELDS}
-  {
-    projects {
-      ...project_data
-    }
-  }
-`;
-
-export const GET_PROJECTS_LANGUAGE = gql`
-  ${PROJECT_FIELDS}
-  query finByLanguage($language: Language = ES) {
-    projectFinByLanguage(language: $language) {
+  query findLanguage($param: Language, $all: Boolean = true) {
+    projects:projectFinByLanguage(language: $param, all: $all) {
       ...project_data
     }
   }
