8df8466cf866b9118d161bc2f1740e8a5ee8cc50
senixcode <codesenix@gmail.com>
2021-03-28 20:51:09 -0500

    feat: :sparkles: added findByLanguage -> aboutMe

--- src/graphql/querys/aboutMeQuery.js
+++ src/graphql/querys/aboutMeQuery.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-boost";
 export const GET_ABOUTME = gql`
-  {
-    allAboutMe {
+  query findLanguage($param: Language, $all: Boolean = true) {
+    allAboutMe:aboutMeFindByLanguage(language: $param, all: $all) {
       id
       name
       language
