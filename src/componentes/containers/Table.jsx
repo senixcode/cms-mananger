00f0e7e992a580573da62f2c525402f5a434a58f
senixcode <codesenix@gmail.com>
2021-03-29 22:31:14 -0500

    feat: :sparkles: added table dynamic

---
+++ src/componentes/containers/Table.jsx
@@ -0,0 +1,45 @@
+export const Table = ({ items }) => (
+  <table className="table mt-3">
+    <thead>
+      <tr>
+        <Header items={items} />
+      </tr>
+    </thead>
+    <tbody>
+      <Body items={items} />
+    </tbody>
+  </table>
+);
+const isNotEmptyArray = (items) => items && items.length > 0;
+const forEachObject = (object, f) =>
+  Object.keys(object).map((key, index) => f(key, index));
+
+const Header = ({ items }) => (
+  <>
+    {isNotEmptyArray(items) ? (
+      forEachObject(items[0], (key, index) => (
+        <th key={index} scope="col">
+          {key}
+        </th>
+      ))
+    ) : (
+      <p> Error table header</p>
+    )}
+  </>
+);
+
+const Body = ({ items }) => (
+  <>
+    {isNotEmptyArray(items) ? (
+      items.map((row, index) => (
+        <tr key={index}>
+          {forEachObject(items[0], (key, i) => (
+            <td key={i}>{row[key]}</td>
+          ))}
+        </tr>
+      ))
+    ) : (
+      <p> Error table body</p>
+    )}
+  </>
+);
