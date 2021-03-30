9dd95984f5b6d16d8255c239e4d12ba20b9476fd
senixcode <codesenix@gmail.com>
2021-03-29 23:45:37 -0500

    feat: :sparkles: added dynamic form and table

--- src/pages/aboutme.mananger.page.jsx
+++ src/pages/aboutme.mananger.page.jsx
@@ -1,19 +1,30 @@
 import { Form } from "../componentes/containers/Form";
 import { Table } from "../componentes/containers/Table";
-const example = [
-{ name: "test", language: "en" },
-{ name: "prueba", language: "es" },
+const formExample = [
+  { type: "textarea", name: "name" },
+  //{ type: "textarea", name: "test" },
+  {
+    type: "select",
+    name: "language",
+    options: [
+      { value: "EN", name: "en" },
+      { value: "ES", name: "es" },
+    ],
+  },
 ];
-export const AboutMeManangerPage = () => {
 
- 
-  return (
-    <div className="card mt-3">
-      <div className="card-header">AboutMe Mananger</div>
-      <div className="card-body">
-        <Form />
-        <Table items={example}/>
-      </div>
+const dataExample = [
+  { name: "test", language: "en" },
+  { name: "prueba", language: "es" },
+  //{ name: "prueba", language: "es" },
+];
+
+export const AboutMeManangerPage = () => (
+  <div className="card mt-3">
+    <div className="card-header">AboutMe Mananger</div>
+    <div className="card-body">
+      <Form inputs={formExample} />
+      <Table items={dataExample} />
     </div>
-  );
-};
+  </div>
+);
