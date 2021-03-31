96de1af43cd635e32273784819ad90908a48e301
senixcode <codesenix@gmail.com>
2021-03-31 13:57:11 -0500

    feat: :sparkles: added param inputs handlesubmit

--- src/componentes/containers/Form.jsx
+++ src/componentes/containers/Form.jsx
@@ -2,21 +2,14 @@ import { useRef } from "react";
 import { isNotEmptyArray } from "../helpers/isNotEmptyArray";
 import { InputSwitch } from "../molecules/InputSwitch";
 
-export const Form = ({ inputs }) => {
+export const Form = ({ inputs,handleSubmit }) => {
   const formRef = useRef();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    inputs.map((input) =>
-      console.log({ [input.name]: formRef.current[input.name].value })
-    );
-    formRef.current.reset();
-  };
 
   return (
     <form className="w-4" ref={formRef}>
       <Inputs elements={inputs} />
-      <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+      <button type="submit" className="btn btn-primary" onClick={(e) => handleSubmit(e,formRef)}>
         Save
       </button>
     </form>
@@ -36,3 +29,11 @@ const Inputs = ({ elements }) => (
     )}
   </>
 );
+
+export const forEachFormRef = (inputs, formRef, f) => {
+  inputs.forEach((i) => {
+    let key = i.name;
+    let value = formRef.current[key].value;
+    return f(key, value);
+  });
+};
