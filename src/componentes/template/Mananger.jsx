74e8b9fe1b0e39b4e3cf268c658e7c95f7c5bb81
senixcode <codesenix@gmail.com>
2021-03-31 13:57:45 -0500

    feat: :sparkles: added  param object form

--- src/componentes/template/Mananger.jsx
+++ src/componentes/template/Mananger.jsx
@@ -1,12 +1,14 @@
 import { Form } from "../containers/Form";
 import { Table } from "../containers/Table";
 
-export const ManangerTemplate = ({title,form,table}) => (
+export const ManangerTemplate = ({title,form,table}) => {
+  const {handleSubmit, inputs} = form
+  return(
   <div className="card mt-3">
     <div className="card-header">{title}</div>
     <div className="card-body">
-      <Form inputs={form} />
+      <Form inputs={inputs} handleSubmit={handleSubmit}/>
       <Table items={table} />
     </div>
   </div>
-);
+)};
