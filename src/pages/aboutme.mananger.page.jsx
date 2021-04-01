ddc79a8062896dbf768d09a149b845524c159f2f
senixcode <codesenix@gmail.com>
2021-04-01 14:52:29 -0500

    feat: :sparkles: added handleEdit

--- src/pages/aboutme.mananger.page.jsx
+++ src/pages/aboutme.mananger.page.jsx
@@ -2,6 +2,7 @@ import { ManangerTemplate } from "../componentes/template/Mananger";
 import { GET_ABOUTME } from "../graphql/querys/aboutMeQuery";
 import { ADD_ABOUTME } from "../graphql/mutation/aboutMeMutation";
 import { useManangerPage } from "../hooks/useManangerPage";
+import { useState } from "react";
 
 const title = "AboutMe Mananger";
 
@@ -17,21 +18,35 @@ const form = [
   },
 ];
 
-const get = { 
-  query:GET_ABOUTME, variables:{param:null}
-}
+const get = {
+  query: GET_ABOUTME,
+  variables: { param: null },
+};
 
 export const AboutMeManangerPage = () => {
-  const {error, loading, data, handleSubmit} = useManangerPage(form,get,ADD_ABOUTME)
+  const { error, loading, data, handleSubmit } = useManangerPage(
+    form,
+    get,
+    ADD_ABOUTME
+  );
+  const [edit, setEdit] = useState({
+    state: false,
+    data: {},
+  });
   if (error) return <p>Error get aboutme graphql</p>;
   if (loading) return <p>Loading aboutme graphql...</p>;
-
+  const handleEdit = (row) => {
+    const {name, language} = row
+    setEdit({
+      state: true,
+      data: {name,language}
+    });
+  };
   return (
     <ManangerTemplate
       title={title}
-      form={{ inputs: [...form], handleSubmit }}
-      table={data.allAboutMe}
+      form={{ inputs: [...form], edit, setEdit, handleSubmit }}
+      table={{ items: [...data.allAboutMe], handleEdit }}
     />
   );
 };
-
