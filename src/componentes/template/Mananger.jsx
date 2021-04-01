57058f74a01a4761ca4ae4b81816bba75310216b
senixcode <codesenix@gmail.com>
2021-04-01 14:50:59 -0500

    refactor: :recycle: refactor form,table

--- src/componentes/template/Mananger.jsx
+++ src/componentes/template/Mananger.jsx
@@ -2,13 +2,14 @@ import { Form } from "../containers/Form";
 import { Table } from "../containers/Table";
 
 export const ManangerTemplate = ({title,form,table}) => {
-  const {handleSubmit, inputs} = form
+  const {handleSubmit, edit, setEdit, inputs} = form
+  const {handleEdit, items} = table
   return(
   <div className="card mt-3">
     <div className="card-header">{title}</div>
     <div className="card-body">
-      <Form inputs={inputs} handleSubmit={handleSubmit}/>
-      <Table items={table} />
+      <Form inputs={inputs} edit={edit} setEdit={setEdit} handleSubmit={handleSubmit}/>
+      <Table items={items} handleEdit={handleEdit}/>
     </div>
   </div>
 )};
