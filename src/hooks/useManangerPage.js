5f5a9df0d242f10c14b26614c6f43f079805e811
senixcode <codesenix@gmail.com>
2021-04-01 22:19:11 -0500

    feat: :sparkles: added deleted dynamic

--- src/hooks/useManangerPage.js
+++ src/hooks/useManangerPage.js
@@ -1,33 +1,32 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { forEachFormRef } from "../componentes/containers/Form";
 // mananger form, table and actions crud
-export const useManangerPage = (form, get, add, update) => {
+export const useManangerPage = (form, get, saved, updated, deleted) => {
   const { query, variables } = get;
   const { error, loading, data } = useQuery(query, { variables });
-  const [Save] = useMutation(add, {
-    //automatic refresh
-    refetchQueries: [{ query, variables }],
-    awaitRefetchQueries: true,
-  });
-  const [Update] = useMutation(update, {
-    //automatic refresh
-    refetchQueries: [{ query, variables }],
-    awaitRefetchQueries: true,
-  });
-  // formRef dynamic
+  const optionsRefresh = OptionsRefresh(query, variables);
+  const [Saved] = useMutation(saved, optionsRefresh);
+  const [Updated] = useMutation(updated, optionsRefresh);
+  const [Deleted] = useMutation(deleted, optionsRefresh);
+  // saved and update dynamic
   const handleSubmit = (e, formRef, edit, setEdit) => {
     e.preventDefault();
     let input = {};
-    if (edit.state) form = [{name: "id" }, ...form];
+    if (edit.state) form = [{ name: "id" }, ...form];
     // parse and get values inputs -> parameters for sending
     forEachFormRef(form, formRef, (key, value) => (input[key] = value));
-    if(edit.state) input.id = parseInt(input.id)
-    input = { variables: {input}}
-    edit.state ? Update(input) : Save(input);
+    if (edit.state) input.id = parseInt(input.id);
+    input = { variables: { input } };
+    edit.state ? Updated(input) : Saved(input);
     // reset edit
     setEdit({ state: false, data: {} });
     //clear form
     formRef.current.reset();
+  };
+
+  const handleDelete = (row) => {
+    const { id } = row;
+    Deleted({ variables: { id } });
   };
 
   return {
@@ -35,5 +34,11 @@ export const useManangerPage = (form, get, add, update) => {
     loading,
     data,
     handleSubmit,
+    handleDelete,
   };
 };
+
+const OptionsRefresh = (query, variables) => ({
+  refetchQueries: [{ query, variables }],
+  awaitRefetchQueries: true,
+});
