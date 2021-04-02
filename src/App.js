f37f51bad8a564be4904ef2a24d7c32c1474d79d
senixcode <codesenix@gmail.com>
2021-04-02 13:38:58 -0500

    feat: :sparkles: added route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router } from "./Route";
+import { Router } from "./componentes/common/Route";
 import { Navigation } from "./componentes/common/Navbar";
 import "bootswatch/dist/darkly//bootstrap.min.css";
 function App() {
