86b0ebeeff8057490ca012783cc1159cf5da06f4
senixcode <codesenix@gmail.com>
2021-04-01 22:19:53 -0500

    feat: :sparkles: added handleDelete

--- src/componentes/template/Mananger.jsx
+++ src/componentes/template/Mananger.jsx
@@ -1,15 +1,25 @@
 import { Form } from "../containers/Form";
 import { Table } from "../containers/Table";
 
-export const ManangerTemplate = ({title,form,table}) => {
-  const {handleSubmit, edit, setEdit, inputs} = form
-  const {handleEdit, items} = table
-  return(
-  <div className="card mt-3">
-    <div className="card-header">{title}</div>
-    <div className="card-body">
-      <Form inputs={inputs} edit={edit} setEdit={setEdit} handleSubmit={handleSubmit}/>
-      <Table items={items} handleEdit={handleEdit}/>
+export const ManangerTemplate = ({ title, form, table }) => {
+  const { handleSubmit, edit, setEdit, inputs } = form;
+  const { items, handleEdit, handleDelete } = table;
+  return (
+    <div className="card mt-3">
+      <div className="card-header">{title}</div>
+      <div className="card-body">
+        <Form
+          inputs={inputs}
+          edit={edit}
+          setEdit={setEdit}
+          handleSubmit={handleSubmit}
+        />
+        <Table
+          items={items}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      </div>
     </div>
-  </div>
-)};
+  );
+};
