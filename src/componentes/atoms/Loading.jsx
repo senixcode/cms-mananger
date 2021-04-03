198cd1455c8a81937ad1f351aff70bb595526fb6
senixcode <codesenix@gmail.com>
2021-04-03 14:55:17 -0500

    feat: :sparkles: added Loading

---
+++ src/componentes/atoms/Loading.jsx
@@ -0,0 +1,10 @@
+export const Loading = () => (
+  <div
+    class="d-flex flex-row justify-content-center align-items-center"
+    style={{height: "100pvh"}}
+  >
+    <div className="spinner-border" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  </div>
+);
