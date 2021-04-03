5bd393a0aab24095706beb8d109a65f1e43fd1a8
senixcode <codesenix@gmail.com>
2021-04-03 15:24:21 -0500

    feat: :sparkles: change SwitchCustome

--- src/componentes/molecules/Filter.jsx
+++ src/componentes/molecules/Filter.jsx
@@ -1,22 +1,15 @@
 import React from "react";
+import { SwitchCustome } from "../atoms/SwitchCustom";
 
 export const Filter = ({ filter, handleChangeFilter }) => (
   <div className="p-3">
-    <div className="form-check form-switch">
-      <input
-        className="form-check-input"
-        type="checkbox"
-        id={filter.name}
-        checked={filter.checked}
-        //   defaultChecked={filter.checked}
-        onChange={(e) => {
-          let checked = e.target.checked;
-          handleChangeFilter({ ...filter, checked });
-        }}
-      />
-      <label className="form-check-label" htmlFor={filter.name}>
-        {filter.name}
-      </label>
-    </div>
+    <SwitchCustome
+      name={filter.name}
+      checked={filter.checked}
+      onChange={(e) => {
+        let checked = e.target.checked;
+        handleChangeFilter({ ...filter, checked });
+      }}
+    />
   </div>
 );
